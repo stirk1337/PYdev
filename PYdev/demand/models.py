06dc72817b4553e48c9fddc22d36a29827d2bd67
Ramil Mavlyutov <stirk2281337@gmail.com>
2023-01-03 17:33:03 +0500

    Добавил базу данных(Востребованность), вывел базу на страницу

--- PYdev/demand/models.py
+++ PYdev/demand/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Demand(models.Model):
+    year = models.IntegerField('Год')
+    salary = models.IntegerField('Уровень зарплат')
+    count = models.IntegerField('Количество')
+    salary_by_prof = models.IntegerField('Уровень зарплат(Python-программист)')
+    count_by_prof = models.IntegerField('Количество (Python-программист)')
+
+    class Meta:
+        verbose_name = 'Статистика вакансий за год'
+        verbose_name_plural = 'Вакансии'
--- PYdev/demand/views.py
+++ PYdev/demand/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Demand
 
 
 def demand(request):
-    return render(request, 'demand/demand.html')
+    data = Demand.objects.all()
+    return render(request, 'demand/demand.html', {'data': data ,
+                                                  'prof': 'Python-программист '})
